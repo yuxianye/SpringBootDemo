40a8036c88337df92e237710209606d81c13f3af
yuxianye <yuxianye@163.com>
2020-08-14 16:39:03 +0800

    增加jsp页面

--- src/main/java/com/yuxianye/mes/controller/UserController.java
+++ src/main/java/com/yuxianye/mes/controller/UserController.java
@@ -3,6 +3,7 @@ package com.yuxianye.mes.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,22 +11,29 @@ import org.springframework.web.bind.annotation.RestController;
 import com.yuxianye.mes.entity.*;
 import com.yuxianye.mes.service.*;
 
-@RestController
+//@RestController
+@Controller
+//@RequestMapping("/user")
 public class UserController {
 
 	@Autowired
 	UserService userService;
 
-	@RequestMapping(value = "/user", method = RequestMethod.GET)
+//	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	@RequestMapping(value = "/user")
+	public String User() {
+
+		return "user";
+
+	}
+
 	public String user(Integer id) {
 
 		Optional<User> user = userService.findById(id);
 
 		return "Hello World!" + user.get().getName();
-		//return "Hello World!" ;
-		
+		// return "Hello World!" ;
+
 	}
-	
-	
-	
+
 }
